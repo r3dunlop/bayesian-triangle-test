29e62669502bdcf716a19e4b7aaecc887240b25d
Ryan Dunlop <rdunlop@uoguelph.ca>
2020-04-07 13:33:33 -0400

    broadcast probability calculation

--- util/stats.py
+++ util/stats.py
@@ -7,9 +7,8 @@ def probability_of_x_differentiators(x, y, n):
     # y = number of correct responses
     # n = total number of tasters
     numerator = comb(n - x, y - x) * np.power(1 / 3., y - x) * np.power(2 / 3., n - y)
-    denominator = 0
-    for u in range(y+1):
-        denominator += comb(n - u, y - u) * np.power(1 / 3., y - u) * np.power(2 / 3., n - y)
+    u = np.arange(0, y + 1 )
+    denominator = np.sum(comb(n - u, y - u) * np.power(1 / 3., y - u) * np.power(2 / 3., n - y))
 
     return numerator / denominator
 
