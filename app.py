abf3ec0d236c68c6252b78675454e3d1c1d2a9b9
Ryan Dunlop <rdunlop@uoguelph.ca>
2020-04-07 11:30:29 -0400

    added probability graph

--- app.py
+++ app.py
@@ -2,45 +2,59 @@ import os
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+from dash.dependencies import Input, Output
 
+import numpy as np
 from util.stats import probability_of_x_differentiators, calculate_pvalue
 
 app = dash.Dash(__name__)
 server = app.server
-colors = {
-    'background' : '#111111',
-    'text' : '#7FDBFF'
-}
 
-app.layout = html.Div(style={'backgroundColor' : colors['background']}, children=[
-                      html.H1(
-                          children = 'Number of discriminators',
-                          style={
-                              'textAlign' : 'center',
-                              'color' : colors['text']
-                          }
-                      ),
-                      html.Div(children='The probability of each discriminator', style={
-                          'textAlign': 'center',
-                          'color': colors['text']
-                      }),
-                      dcc.Graph(
-                          id='Graph1',
-                          figure={
-                              'data': [
-                                  {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
-                                  {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
-                              ],
-                              'layout': {
-                                  'plot_bgcolor': colors['background'],
-                                  'paper_bgcolor': colors['background'],
-                                  'font': {
-                                      'color': colors['text']
-                                  }
-                              }
-                          }
-                      )]
-                )
+app.layout = html.Div(children=[
+                html.H1(
+                    children='Number of discriminators',
+                ),
+                html.H3(children='The probability of number of discriminators',),
+                html.Div([
+                    html.Div([
+                        'Number of Tasters: ',
+                        dcc.Input(id='input-tasters', type='number', value='24'),
+                    ],
+                        style=dict(
+                            width='30%',
+                        ),
+                    ),
+                    html.Div([
+                        'Number of Correct: ',
+                        dcc.Input(id='input-correct', type='number', value='12'),
+                    ],
+                        style=dict(
+                            width='30%',
+                        ),
+                    ),
+                ]),
+                dcc.Graph(
+                      id='graph-probability-differentiators',
+                      figure={
+                          'data': [
+                              {'x': np.arange(0,24), 'y': probability_of_x_differentiators(np.arange(0,24),12,24), 'type': 'bar', 'name': 'Differentiators'},
+                          ],
+
+
+
+                      }
+                  )]
+            )
+
+@app.callback(Output('graph-probability-differentiators', 'figure'),
+              [Input('input-tasters', 'value'),
+               Input('input-correct','value')])
+def update_differentiator_graph(n, y):
+    y = int(y)
+    n = int(n)
+    return {'data': [{'x': np.arange(0,n), 'y': probability_of_x_differentiators(np.arange(0,n),y,n), 'type': 'bar', 'name': 'Differentiators'},]}
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
