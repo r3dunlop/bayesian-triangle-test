3c614bbbc2a41bc64cd48de57e2b437678c35d9c
Ryan Dunlop <rdunlop@uoguelph.ca>
2020-04-07 15:19:56 -0400

    Added proportion option

--- app.py
+++ app.py
@@ -33,6 +33,18 @@ app.layout = html.Div(children=[
                         ),
                     ),
                 ]),
+                dcc.Dropdown(
+                    id='dd-graph-type',
+                    options=[
+                        {'label': 'Number of Discriminators', 'value': 'NUMBER'},
+                        {'label': 'Proportion of Discriminators', 'value': 'PROPORTION'},
+                    ],
+                    value='NUMBER',
+                    clearable=False,
+                    style=dict(
+                        width='50%',
+                    ),
+                ) ,
                 html.H3('p-value: ',id='pvalue'),
                 dcc.Graph(
                       id='graph-probability-differentiators',
@@ -44,7 +56,7 @@ app.layout = html.Div(children=[
                           'layout': {
                               'title': 'Probability of the number of discriminators',
                               'yaxis': {'title': 'Probability', 'autorange': True},
-                              'xaxis': {'title': 'Number of Differentiators', 'autorange': True},
+                              'xaxis': {'title': 'Number of Discriminators', 'autorange': True},
                               'bargap':0,
 
                           }
@@ -91,17 +103,34 @@ def update_pvalue(n, y):
 @app.callback([Output('graph-probability-differentiators', 'figure'),
                Output('graph-cumulative-probability-differentiators', 'figure')],
               [Input('input-tasters', 'value'),
-               Input('input-correct','value')],
+               Input('input-correct','value'),
+               Input('dd-graph-type','value')],
               [State('graph-probability-differentiators', 'figure'),
                State('graph-cumulative-probability-differentiators', 'figure')]
               )
-def update_differentiator_graph(n, y, pdf, cdf):
+def update_differentiator_graph(n, y, dd_val, pdf, cdf,):
     y = clean_integer(y)
     n = clean_integer(n)
-    pdf['data'] = [{'x': np.arange(0, n), 'y': probability_of_x_differentiators(np.arange(0, n), y, n), 'type': 'bar',
+
+    divisor = n
+    mapping = ('number','proportion')
+    if dd_val == 'NUMBER':
+        divisor = 1
+        mapping = ('proportion', 'number')
+
+
+    pdf['data'] = [{'x': np.arange(0, n)/divisor, 'y': probability_of_x_differentiators(np.arange(0, n), y, n), 'type': 'bar',
                'name': 'Differentiators'}]
-    cdf['data'] = [{'x': np.arange(0, n), 'y': probability_of_more_than_x_differentiators(np.arange(0, n), y, n), 'type': 'bar',
+
+    cdf['data'] = [{'x': np.arange(0, n)/divisor, 'y': probability_of_more_than_x_differentiators(np.arange(0, n), y, n), 'type': 'bar',
          'name': 'Differentiators','marker':{ "color" : ['crimson']*n}},]
+
+    for graph in [pdf,cdf]:
+        graph['layout']['title'] = graph['layout']['title'].replace(*mapping)
+        graph['layout']['title'] = graph['layout']['title'].replace(*map(lambda x: x.title(),mapping))
+        graph['layout']['xaxis']['title']['text'] = graph['layout']['xaxis']['title']['text'].replace(*mapping)
+        graph['layout']['xaxis']['title']['text'] = graph['layout']['xaxis']['title']['text'].replace(*map(lambda x: x.title(),mapping))
+
     return pdf, cdf
 
 if __name__ == '__main__':
